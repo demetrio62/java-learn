8b267ba1e99759217c06907e1ac5f3c30bbce3db
Dmitry Smith <4demetrio@gmail.com>
2019-01-03 21:16:33 +0300

    Adding math operations

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,21 @@
 public class Calculate {
 	public static void main (String[] args){
-		System.out.println("Ready!");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		
+		System.out.println("Ready to Calculate number " + first + " and number " + second + ":");
+		
+		int sum = first + second;
+		int diff = first - second;
+		int mult = first * second;
+		int del = first / second;
+		double lvl = Math.pow(first, second);
+		
+		System.out.println("Сумма: " + sum);
+		System.out.println("Разница: " + diff);
+		System.out.println("Произведение: " + mult);
+		System.out.println("Деление: " + del);
+		System.out.println("Степень: " + lvl);
+				
 	}
 }
